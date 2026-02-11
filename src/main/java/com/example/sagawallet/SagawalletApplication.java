fec53d0222a29eec48e81408a94bdd0e5957e00e
Fahad Mubeen <fahad660606@gmail.com>
2026-02-11 16:48:45 +0530

    first commit

---
+++ src/main/java/com/example/sagawallet/SagawalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagawallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagawalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagawalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sagawallet/SagawalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sagawallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagawalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
