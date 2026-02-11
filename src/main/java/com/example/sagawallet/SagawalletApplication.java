a265890127193b32f7f0e3ccce9f506c9ce16018
Fahad Mubeen <fahad660606@gmail.com>
2026-02-11 20:47:12 +0530

    feat(sharding): configure env var support and add User entity for testing

--- src/main/java/com/example/sagawallet/SagawalletApplication.java
+++ src/main/java/com/example/sagawallet/SagawalletApplication.java
@@ -10,6 +10,11 @@ public class SagawalletApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		System.getenv().forEach((key, value) -> {
+			if (System.getProperty(key) == null) {
+				System.setProperty(key, value);
+			}
+		});
 		SpringApplication.run(SagawalletApplication.class, args);
 	}
 
