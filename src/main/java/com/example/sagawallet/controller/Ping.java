3ee43bbe666175a966e51796275567654664d3f6
Fahad Mubeen <fahad660606@gmail.com>
2026-02-22 13:02:17 +0530

    created wallet controller

--- src/main/java/com/example/sagawallet/controller/Ping.java
+++ src/main/java/com/example/sagawallet/controller/Ping.java
@@ -1,11 +1,11 @@
 package com.example.sagawallet.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/ping")
 public class Ping {
     @GetMapping
---
+++ src/main/java/com/example/sagawallet/controller/WalletController.java
@@ -0,0 +1,51 @@
+package com.example.sagawallet.controller;
+
+import com.example.sagawallet.dto.CreateWalletRequestDTO;
+import com.example.sagawallet.entity.Wallet;
+import com.example.sagawallet.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/wallets")
+@RequiredArgsConstructor
+@Slf4j
+public class WalletController {
+    private final WalletService walletService;
+
+    @PostMapping
+    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletRequestDTO request) {
+        Wallet wallet = walletService.createWallet(request.getUserId());
+        return ResponseEntity.ok(wallet);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Wallet> getWalletById(@PathVariable Long id) {
+        Wallet wallet = walletService.getWalletById(id);
+        return ResponseEntity.ok(wallet);
+    }
+
+    @GetMapping("/balance/{id}")
+    public ResponseEntity<BigDecimal> getBalance(@PathVariable Long id) {
+        BigDecimal balance = walletService.getBalance(id);
+        return ResponseEntity.ok(balance);
+    }
+
+    @PostMapping("/debit/{id}")
+    public ResponseEntity<Wallet> debit(@PathVariable Long id, @RequestParam BigDecimal amount) {
+        walletService.debit(id, amount);
+        Wallet wallet = walletService.getWalletById(id);
+        return ResponseEntity.ok(wallet);
+    }
+
+    @PostMapping("/credit/{id}")
+    public ResponseEntity<Wallet> credit(@PathVariable Long id, @RequestParam BigDecimal amount) {
+        walletService.credit(id, amount);
+        Wallet wallet = walletService.getWalletById(id);
+        return ResponseEntity.ok(wallet);
+    }
+}
